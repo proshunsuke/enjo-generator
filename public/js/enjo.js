00662c8c86d71c9170f97bfedf42dc7070b167d9
proshunsuke <shunsuke0901@gmail.com>
2014-10-30 08:57:24 +0900

    画像保存

--- public/js/enjo.js
+++ public/js/enjo.js
@@ -65,31 +65,55 @@
                 this.$data.enjoMsg2 = "もし炎上したら　　　　木の下に埋めて貰っても構わないよ";
                 this.inputMsg();
             },
-            save: function(){
-                this.saveBlob(this.dataUrlToBlob(cvs.toDataURL()),'enjo.png');
+            // thanks to http://jsdo.it/Yukisuke/p311
+            ArraytoBlob: function(_mime,_array)
+            {
+                // ArrayBufferやUint8Arrayなら入れなおす工数がなくなります
+                var arr = new Uint8Array(_array.length);
+                for (var i = 0; i < _array.length; i++) {arr[i] = _array[i];}
+
+                var blob = new Blob([arr], { type: _mime });
+                return blob;
             },
-            // thanks to http://www55.atpages.jp/triplog/pg/monthly/1405/ra-men.js?0526
-            saveBlob: function(blob, name){
-//     var url = parent.URL || parent.webkitURL,
-                var url = window.URL || window.webkitURL,
-                    objectUrl = url.createObjectURL(blob),
-                    e = new Event('click'),
-                    el = document.createElement('a');
-                el.href = objectUrl;
-                el.download = name;
-                el.dispatchEvent(e);
-            },
-            // thanks to http://www55.atpages.jp/triplog/pg/monthly/1405/ra-men.js?0526
-            dataUrlToBlob: function(dataUrl){
-                var splitted = dataUrl.split(','),
-                    byteString = atob(splitted[1]),
-                    mimeString = splitted[0].split(':')[1].split(';')[0],
-                    ab = new ArrayBuffer(byteString.length),
-                    ia = new Uint8Array(ab);
-                for (var i = 0; i < byteString.length; i++) {
-                    ia[i] = byteString.charCodeAt(i);
+            // thanks to http://jsdo.it/Yukisuke/p311
+            saveBlob: function(_blob,_file)
+            {
+                if( /*@cc_on ! @*/ false )
+                {	// IEの場合
+                    window.navigator.msSaveBlob(_blob, _file);
                 }
-                return new Blob([ia], { type: mimeString });
+                else
+                {
+                    var url = (window.URL || window.webkitURL);
+                    var data = url.createObjectURL(_blob);
+                    var e = document.createEvent("MouseEvents");
+                    e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                    var a = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+                    a.href = data;
+                    a.download = _file;
+                    a.dispatchEvent(e);
+                }
+            },
+            // thanks to http://jsdo.it/Yukisuke/p311
+            Base64toBlob: function(_base64)
+            {
+                var i;
+                var tmp = _base64.split(',');
+                var data = atob(tmp[1]);
+                var mime = tmp[0].split(':')[1].split(';')[0];
+
+                //var buff = new ArrayBuffer(data.length);
+                //var arr = new Uint8Array(buff);
+                var arr = new Uint8Array(data.length);
+                for (i = 0; i < data.length; i++) {arr[i] = data.charCodeAt(i);}
+                var blob = new Blob([arr], { type: mime });
+                return blob;
+            },
+            // thanks to http://jsdo.it/Yukisuke/p311
+            save: function(){
+                var base64 = cvs.toDataURL();    // firfoxならtoblobで直接blobにして保存できます。
+                var blob = this.Base64toBlob(base64);
+                this.saveBlob(blob,"enjo.png");
             }
         }
     });
